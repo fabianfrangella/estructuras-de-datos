4eb963c7d88ec50cb45cebf4d5b3c9d99c21bd99
Andres Mora <andeehr@gmail.com>
2021-04-28 23:21:39 -0300

    tesoros

--- Andi/practica4.hs
+++ Andi/practica4.hs
@@ -64,3 +64,70 @@ duplicar ing = ing
 cantCapasPorPizza :: [Pizza] -> [(Int, Pizza)]
 cantCapasPorPizza [] = []
 cantCapasPorPizza (x:xs) = (cantidadDeCapas x, x) : cantCapasPorPizza xs
+
+--2. Mapa de tesoros (con bifurcaciones)
+--Un mapa de tesoros es un árbol con bifurcaciones que terminan en cofres. Cada bifurcación y
+--cada cofre tiene un objeto, que puede ser chatarra o un tesoro.
+data Dir = Izq | Der deriving Show
+
+data Objeto = Tesoro | Chatarra deriving Show
+
+data Cofre = Cofre [Objeto] deriving Show
+
+data Mapa = Fin Cofre
+            | Bifurcacion Cofre Mapa Mapa deriving Show
+
+mapaTesoro = Bifurcacion (Cofre []) 
+				(Bifurcacion (Cofre []) 
+					(Bifurcacion (Cofre []) 
+						(Fin (Cofre [])) 
+						(Fin (Cofre [Tesoro])))
+					(Fin (Cofre [])))
+				(Fin (Cofre []))
+
+mapaSinTesoro = Bifurcacion (Cofre []) 
+				(Bifurcacion (Cofre [Chatarra]) 
+					(Fin (Cofre []))
+					(Fin (Cofre [])))
+				(Fin (Cofre []))
+
+mapaLargo = Bifurcacion (Cofre [Tesoro]) 
+				(Bifurcacion (Cofre []) 
+					(Bifurcacion (Cofre []) 
+						(Bifurcacion (Cofre []) 
+							(Fin(Cofre []))
+							(Bifurcacion (Cofre []) 
+								(Fin(Cofre []))
+								(Fin(Cofre [])))) 
+						(Fin (Cofre [Tesoro])))
+					(Fin (Cofre [])))
+				(Fin (Cofre [Tesoro]))
+
+--Definir las siguientes operaciones:
+--Indica si hay un tesoro en alguna parte del mapa.
+hayTesoro :: Mapa -> Bool
+hayTesoro (Fin c) = hayTesoroEnCofre c
+hayTesoro (Bifurcacion c m1 m2) = hayTesoroEnCofre c || hayTesoro m1 || hayTesoro m2
+
+hayTesoroEnCofre :: Cofre -> Bool
+hayTesoroEnCofre (Cofre o) = hayTesoroEnObjetos o
+
+hayTesoroEnObjetos :: [Objeto] -> Bool
+hayTesoroEnObjetos [] = False
+hayTesoroEnObjetos (x:xs) = esTesoro x || hayTesoroEnObjetos xs
+
+esTesoro :: Objeto -> Bool
+esTesoro Tesoro = True
+esTesoro _ = False
+
+--2. hayTesoroEn :: [Dir] -> Mapa -> Bool
+--Indica si al final del camino hay un tesoro. Nota: el final de un camino se representa con una
+--lista vacía de direcciones.
+--3. caminoAlTesoro :: Mapa -> [Dir]
+--Indica el camino al tesoro. Precondición: existe un tesoro y es único.
+--4. caminoDeLaRamaMasLarga :: Mapa -> [Dir]
+--Indica el camino de la rama más larga.
+--5. tesorosPorNivel :: Mapa -> [[Objeto]]
+--Devuelve los tesoros separados por nivel en el árbol.
+--6. todosLosCaminos :: Mapa -> [[Dir]]
+--Devuelve todos lo caminos en el mapa.
