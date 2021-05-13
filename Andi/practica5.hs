40ed62957ab54624839719fcdb06d10c6db541c4
Andres Mora <andeehr@gmail.com>
2021-05-13 18:17:41 -0300

    enunciado parte 2

--- Andi/practica5.hs
+++ Andi/practica5.hs
@@ -83,3 +83,50 @@ ordenar [] = []
 orderar xs =
     let m = minimo xs
         in m : ordenar (sacar m xs)
+
+--2. Set (conjunto)
+--Un Set es un tipo abstracto de datos que consta de las siguientes operaciones:
+
+data Set a = ConsS [a]
+
+--Crea un conjunto vacío.
+emptyS :: Set a
+
+--Dados un elemento y un conjunto, agrega el elemento al conjunto.
+addS :: Eq a => a -> Set a -> Set a
+
+--Dados un elemento y un conjunto indica si el elemento pertenece al conjunto.
+belongs :: Eq a => a -> Set a -> Bool
+
+--Devuelve la cantidad de elementos distintos de un conjunto.
+sizeS :: Eq a => Set a -> Int
+
+--Borra un elemento del conjunto.
+removeS :: Eq a => a -> Set a -> Set a
+
+--Dados dos conjuntos devuelve un conjunto con todos los elementos de ambos. conjuntos.
+unionS :: Eq a => Set a -> Set a -> Set a
+
+--Dado un conjunto devuelve una lista con todos los elementos distintos del conjunto.
+setToList :: Eq a => Set a -> [a]
+
+--1. Implementar la variante del tipo abstracto Set con una lista que no tiene repetidos y guarda
+--la cantidad de elementos en la estructura.
+--Nota: la restricción Eq aparece en toda la interfaz se utilice o no en todas las operaciones
+--de esta implementación, pero para mantener una interfaz común entre distintas posibles
+--implementaciones estamos obligados a escribir así los tipos.
+--2. Como usuario del tipo abstracto Set implementar las siguientes funciones:
+
+--Dados una lista y un conjunto, devuelve una lista con todos los elementos que pertenecen al conjunto.
+losQuePertenecen :: Eq a => [a] -> Set a -> [a]
+
+--Quita todos los elementos repetidos de la lista dada utilizando un conjunto como estructura auxiliar.
+sinRepetidos :: Eq a => [a] -> [a]
+
+--Dado un arbol de conjuntos devuelve un conjunto con la union de todos los conjuntos del arbol.
+unirTodos :: Eq a => Tree (Set a) -> Set a
+
+--3. Implementar la variante del tipo abstracto Set que posee una lista y admite repetidos. En
+--otras palabras, al agregar no va a chequear que si el elemento ya se encuentra en la lista, pero
+--sí debe comportarse como Set ante el usuario (quitando los elementos repetidos al pedirlos,
+--por ejemplo). Contrastar la eficiencia obtenida en esta implementación con la anterior.
