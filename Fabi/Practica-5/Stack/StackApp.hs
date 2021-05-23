9e13a89a33773e948f8e031c2eb69f0f6bedaad4
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-23 14:49:09 -0300

    costos

--- Fabi/Practica-5/Stack/StackApp.hs
+++ Fabi/Practica-5/Stack/StackApp.hs
@@ -1,11 +1,13 @@
 import Stack1 
 
+-- O (n)
 --Como usuario del tipo abstracto Stack implementar las siguientes funciones:
 --Dada una lista devuelve una pila sin alterar el orden de los elementos.
 apilar :: [a] -> Stack a
 apilar [] = emptyS
 apilar (x:xs) = push x (apilar xs)
 
+-- O(n2)
 --Dada una pila devuelve una lista sin alterar el orden de los elementos.
 desapilar :: Stack a -> [a]
 desapilar s = 
@@ -13,6 +15,7 @@ desapilar s =
         then []
         else top s : desapilar (pop s) 
 
+-- O(n)
 --Dada una posicion válida en la stack y un elemento, ubica dicho elemento en dicha
 --posición (se desapilan elementos hasta dicha posición y se inserta en ese lugar).
 insertarEnPos :: Int -> a -> Stack a -> Stack a
