72c15b6cc747ce9ad603bd36fc25cef262e8fcee
Andrés Mora <andeehr@gmail.com>
2021-06-03 11:45:38 -0300

    autores sin usuario

---
+++ Andi/Parciales-viejos/Autores/Map1.hs
@@ -0,0 +1,62 @@
+module Map1
+  (
+    Map,
+    emptyM,
+    assocM,
+    lookupM,
+    domM
+  ) where
+
+--2. Map (diccionario)
+
+--Ejercicio 3
+--La interfaz del tipo abstracto Map es la siguiente:
+
+--1. Como una lista de pares-clave valor sin claves repetidas 
+data Map k v = M [(k, v)] deriving Show
+
+--Propósito: devuelve un map vacío
+emptyM :: Map k v
+emptyM = M []
+
+--Propósito: agrega una asociación clave-valor al map.
+assocM :: Eq k => k -> v -> Map k v -> Map k v
+assocM k v (M xs) = M (replace k v xs)
+
+-- Private
+replace :: Eq k => k -> v -> [(k,v)] -> [(k,v)]
+replace k v [] = [(k,v)]
+replace k v (x:xs) =
+    if k == fst x
+        then (k,v) : xs
+        else x : replace k v xs
+
+--Propósito: encuentra un valor dado una clave.
+lookupM :: Eq k => k -> Map k v -> Maybe v
+lookupM k (M xs) = find k xs
+
+find :: Eq k => k -> [(k, v)] -> Maybe v
+find k [] = Nothing
+find k (x:xs) =
+    if k == fst x
+        then Just (snd x)
+        else find k xs
+
+--Propósito: borra una asociación dada una clave.
+deleteM :: Eq k => k -> Map k v -> Map k v
+deleteM k (M xs) = M (delete k xs)
+
+delete :: Eq k => k -> [(k, v)] -> [(k, v)]
+delete k [] = error "no esite"
+delete k (x:xs) =
+    if k == fst x
+        then xs
+        else x : delete k xs
+
+--Propósito: devuelve las claves del map.
+domM :: Map k v -> [k]
+domM (M xs) = domM' xs
+
+domM' :: [(k, v)] -> [k]
+domM' [] = []
+domM' (x:xs) = fst x : domM' xs
